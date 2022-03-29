b69b4b926519b9b454ebdefa513477d5c651ca7b
Andrey Grytsenko <andreyapg@gmail.com>
2022-03-29 17:09:28 +0300

    12 Meet How to use Inertia together with Vue 2022-03-29

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,35 +1,64 @@
 <template>
+    <Head title="Welcome"/>
+
+    <Section class="grid grid-cols-2 text-right bg-gray-800 text-gray-300">
+        <div>
+            <jet-application-mark class="h-12 w-auto"></jet-application-mark>
+        </div>
+        <div v-if="canLogin">
+            <Link v-if="$page.props.user" :href="route('dashboard')" class="text-sm underline">
+                Dashboard
+            </Link>
+
+            <template v-else>
+                <Link :href="route('login')" class="text-base underline">
+                    Log in
+                </Link>
+
+                <Link v-if="canRegister" :href="route('register')" class="text-base underline place-self-end ml-4">
+                    Register
+                </Link>
+            </template>
+        </div>
+    </Section>
+
     <div>
-        <section class="bg-gray-800 pt-16 h-screen px-72">
+        <Section class="bg-gray-800 pt-16 h-screen">
             <div class="h-2/3 flex flex-wrap content-between pb-36">
-                <p class="border-b-2 font-bold border-gray-400 pb-3 text-2xl text-gray-300 uppercase">Hey! This is Andrey. I'm PHP developer and I would be glad to work with you.</p>
+                <p class="border-b-2 font-bold border-gray-400 pb-3 text-2xl text-gray-300 uppercase">Hey! This is
+                    Andrey. I'm PHP developer and I would be glad to work with you.</p>
             </div>
-        </section>
-        <section class="bg-gray-200 text-gray-800 px-72 py-10 h-screen">
+        </Section>
+        <Section class="bg-gray-200 text-gray-800 h-screen">
             <h2 class="text-6xl font-bold pt-3">Skills</h2>
-        </section>
-        <section class="bg-gray-600 text-gray-200 px-72 py-10 h-screen">
+        </Section>
+        <Section class="bg-gray-600 text-gray-200 h-screen">
             <h2 class="text-6xl font-bold pt-3">Projects</h2>
-        </section>
-        <section class="flex justify-between py-10 px-72 bg-gray-800 text-gray-300 text-xl">
+        </Section>
+        <Section class="flex justify-between bg-gray-800 text-gray-300 text-xl">
             <p>&copy; APG All rights reserved</p>
             <div class="flex justify-evenly items-center">Github Twitter Linkedin</div>
-        </section>
+        </Section>
     </div>
 </template>
 
 <script>
-    import { defineComponent } from 'vue'
-    import { Head, Link } from '@inertiajs/inertia-vue3';
+import {defineComponent} from 'vue'
+import {Head, Link} from '@inertiajs/inertia-vue3';
+import JetApplicationMark from '@/Jetstream/ApplicationMark';
+import Section from '@/Components/Section';
 
-    export default defineComponent({
-        components: {
-            //
-        },
+export default defineComponent({
+    components: {
+        Head,
+        Link,
+        JetApplicationMark,
+        Section
+    },
 
-        props: {
-            canLogin: Boolean,
-            canRegister: Boolean
-        }
-    })
+    props: {
+        canLogin: Boolean,
+        canRegister: Boolean
+    }
+})
 </script>
