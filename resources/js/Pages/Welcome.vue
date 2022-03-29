286e208bdc0d28514a844699eed793ef30c04709
Andrey Grytsenko <andreyapg@gmail.com>
2022-03-29 16:39:18 +0300

    11 Meet Tailwind CSS 2022-03-29

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,5 +1,21 @@
 <template>
-    <div>Welcome</div>
+    <div>
+        <section class="bg-gray-800 pt-16 h-screen px-72">
+            <div class="h-2/3 flex flex-wrap content-between pb-36">
+                <p class="border-b-2 font-bold border-gray-400 pb-3 text-2xl text-gray-300 uppercase">Hey! This is Andrey. I'm PHP developer and I would be glad to work with you.</p>
+            </div>
+        </section>
+        <section class="bg-gray-200 text-gray-800 px-72 py-10 h-screen">
+            <h2 class="text-6xl font-bold pt-3">Skills</h2>
+        </section>
+        <section class="bg-gray-600 text-gray-200 px-72 py-10 h-screen">
+            <h2 class="text-6xl font-bold pt-3">Projects</h2>
+        </section>
+        <section class="flex justify-between py-10 px-72 bg-gray-800 text-gray-300 text-xl">
+            <p>&copy; APG All rights reserved</p>
+            <div class="flex justify-evenly items-center">Github Twitter Linkedin</div>
+        </section>
+    </div>
 </template>
 
 <script>
