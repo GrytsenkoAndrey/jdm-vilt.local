376bd412debcf1664b0c6e2898ad43e4f9d6ca48
Andrey Grytsenko <andreyapg@gmail.com>
2022-03-31 17:40:10 +0300

    26 Allow to Create skill with Laravel and Inertia 2022-03-31

--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Skill;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use Inertia\Inertia;
 
 class SkillController extends Controller
@@ -11,7 +12,20 @@ class SkillController extends Controller
     public function index()
     {
         return Inertia::render('Skills/All', [
-            'skills' => Skill::all()
+            'skills' => Skill::all(),
+            'availableColors' => Skill::getAvailableBackgroundColors()
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'max:255', Rule::unique(Skill::class)],
+            'color' => ['required', 'in:' . implode(',', Skill::getAvailableBackgroundColors())]
+        ]);
+
+        Skill::create($request->all());
+
+        return redirect()->route('skills.index');
     }
 }
